c12475064bcb675976ba64a5c1dde69cac554a61
BrknHnds <bsbd42@yandex.ru>
2024-01-23 08:27:08 +0500

    feat: add error catcher to 01-read-file 2024-01-23 Tue 08:27:08

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,3 +3,6 @@ const readStream = fs.createReadStream('./01-read-file/text.txt');
 readStream.on('data', (dataPart) => {
   console.log(dataPart.toString());
 })
+readStream.on('error', (err) => {
+  console.log(err);
+})
